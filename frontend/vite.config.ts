57dd8f96dd6487279bc25286a68161484458ee50
elinaldo-santos <elinaldoahff@gmail.com>
2026-09-04 18:33:41 -0300

    v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/app.js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+  },
   server: {
     port: 5173,
     open: true,
